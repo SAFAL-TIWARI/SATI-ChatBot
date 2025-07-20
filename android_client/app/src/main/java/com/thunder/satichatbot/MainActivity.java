d4a33dc08a925433ad97690f7ff62f4a7c1da6d8
thunder-thigh <aashubaghel08@gmail.com>
2025-07-20 17:16:34 +0530

    android_client added

---
+++ android_client/app/src/main/java/com/thunder/satichatbot/MainActivity.java
@@ -0,0 +1,47 @@
+package com.thunder.satichatbot;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mWebView = new WebView(this);
+        setContentView(mWebView);
+
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setSupportMultipleWindows(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setBuiltInZoomControls(false);
+
+        mWebView.setWebViewClient(new WebViewClient()); // Handles in-app loading
+        mWebView.setWebChromeClient(new WebChromeClient()); // Enables JS dialogs etc.
+
+        mWebView.loadUrl("https://sati-chatbot.vercel.app/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
